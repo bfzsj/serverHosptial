b3514c1232d924a219da866f51b172c395772cba
xiaodong.shi <xiaodong.shi@zhaopin.com.cn>
2019-12-27 18:59:23 +0800

    增加查询预约表的接口

--- routes/index.js
+++ routes/index.js
@@ -45,7 +45,9 @@ router.post('/findDoctor',function (req,res) {
     })
 });
 router.get("/getOpenId",function (req,res) {
-    let {code,appid,secret}=req.query;
+    let {code,appid,secret,userInfo}=req.query;
+    userInfo=JSON.parse(userInfo)
+
     try {
         const options={
             method:'GET',
@@ -58,18 +60,68 @@ router.get("/getOpenId",function (req,res) {
             }
         };
         rp(options).then(function (response) {
-            console.log(`ServerB responded with statuscode ${response.statusCode}`)
-            let data=JSON.parse(response);
-            res.send(data.openid)
 
+            let data=JSON.parse(response);
+
+            return data
+
+        }).then((data)=>{
+
+            connection.query("select * from username where wx_open_id='"+data.openid+"'" ,(err,se)=>{
+                if(err){
+                    throw err;
+                }else {
+
+                    if(se.length==0){
+                        connection.query("insert into username(imgUrl,username,password,phone,wx_open_id) values('"+userInfo.avatarUrl+"','"+userInfo.nickName+"','123456','123','"+data.openid+"')" ,(err,datas)=>{
+                            if(err){
+                                throw err;
+                            }else {
+                                let json={
+                                    wx_open_id:data.openid,
+                                    username:userInfo.nickName,
+                                    imgUrl:userInfo.avatarUrl
+                                }
+                                res.send([json]);
+                            }
+                        })
+                    }else{
+                        res.send(se)
+                    }
+
+                }
+            })
         }).catch(function (err) {
-            console.log(`ServerB responded with error ${err}`)
+
         })
 
     }catch(e) {
 
     }
 });
+router.post('/insertAppointment',(req,res)=>{
+    let {doctorid,user_wx_id,year,month,day,time}=req.body;
+    connection.query("insert into appointment(doctorid,user_wx_id,year,month,day,time) values(?,?,?,?,?,?)" ,[doctorid,user_wx_id,year,month,day,time],(err,datas)=>{
+        if(err){
+            throw err;
+        }else {
+
+            res.send("成功");
+        }
+    })
+})
+router.post('/findOneAppointment',(req,res)=>{
+    let {user_wx_id}=req.body;
+    console.log(user_wx_id)
+    connection.query("select * from appointment where user_wx_id= ?" ,[user_wx_id],(err,datas)=>{
+        if(err){
+            throw err;
+        }else {
+            console.log(datas)
+            res.send(datas);
+        }
+    })
+})
 module.exports = router;
 
   //  'https://api.weixin.qq.com/sns/jscode2session?grant_type=authorization_code&js_code=061h2AF70fIBpF1SBuH70JxEF70h2AFO&secret=0d8949e5dfb19aaefd8f568cdc5d4cdc&appid=wx7d118a6b1b628f42'
